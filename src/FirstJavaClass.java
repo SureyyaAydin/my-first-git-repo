db8b33b7a136c45043c01b7efc74b8e886a5a933
SureyyaAydin <sur.aydin11@gmail.com>
2022-03-26 22:59:09 +0300

    second commit

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -3,5 +3,8 @@ public class FirstJavaClass {
     public static void main(String[] args) {
 
         System.out.println("Learning github");
+        System.out.println("Adding a new code");
+
+
     }
 }
